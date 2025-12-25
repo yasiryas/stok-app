794ae7d91961063aee901fadbb3f731e81ff14dd
Muhammad Yasir <yasir123983@gmail.com>
2025-12-25 20:35:41 +0700

    Add mobile view, search, edit, delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,4 +12,48 @@ class ProductController extends Controller
         $products = Product::latest()->get();
         return view('products.index', compact('products'));
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nama' => 'required',
+            'stok' => 'required|integer|min:0',
+        ]);
+
+        $lastProduct = Product::latest()->first();
+
+        $nextNumber = $lastProduct ? ((int)substr($lastProduct->kode, 3)) + 1 : 1;
+        $kode = 'PRD' . str_pad($nextNumber, 4, '0', STR_PAD_LEFT);
+
+        $product = Product::create([
+            'kode' => $kode,
+            'nama' => $validated['nama'],
+            'stok' => $validated['stok'],
+        ]);
+
+        return response()->json(['message' => 'Product added successfully!', 'product' => $product]);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $validated = $request->validate([
+            'nama' => 'required',
+            'stok' => 'required|integer|min:0',
+        ]);
+
+        $product->update($validated);
+
+        return response()->json(['message' => 'Product updated successfully!', 'product' => $product]);
+    }
+
+    public function edit(Product $product)
+    {
+        return response()->json($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully!']);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'index']);
 Route::resource('products', ProductController::class);
+
+Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
